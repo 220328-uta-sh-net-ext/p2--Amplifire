bafe4dd97fa47f96501eea2c915d14585b75e730
Harsh4598 <harsh.ht008@gmail.com>
2022-05-25 17:35:57 -0400

    delted some file

--- Parlezentree_Backend/ParlezentreeApi/Program.cs
+++ Parlezentree_Backend/ParlezentreeApi/Program.cs
@@ -1,4 +1,8 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.EntityFrameworkCore;
+
+using ParlezentreeDl;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -6,14 +10,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ParlezentreeDl.Entities.parlez_entreeContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ParlezEntreeDb")));
+builder.Services.AddScoped<IUserRepository,UserRepository>();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(); 
 }
 
 app.UseHttpsRedirection();
---
+++ Parlezentree_Backend/ParlezentreeDl/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using ParlezentreeDl.Entities;
+
+namespace ParlezentreeDl;
+public class UserRepository : IUserRepository
+{
+    private parlez_entreeContext db;
+    public UserRepository(parlez_entreeContext db)
+    {
+        this.db = db;
+    }
+    public string addUser(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+
+
+    public List<User> getAllUser()
+    {
+        return db.Users.ToList();
+    }
+}
+
