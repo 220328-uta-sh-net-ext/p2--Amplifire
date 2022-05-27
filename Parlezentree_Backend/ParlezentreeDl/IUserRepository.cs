dc999398c42c037eb34a3fc25c4acc902e549501
Harsh4598 <harsh.ht008@gmail.com>
2022-05-27 17:23:14 -0400

    remove some codesmells and add summery to methods

--- Parlezentree_Backend/ParlezentreeDl/IUserRepository.cs
+++ Parlezentree_Backend/ParlezentreeDl/IUserRepository.cs
@@ -5,10 +5,24 @@ namespace ParlezentreeDl
 {
     public interface IUserRepository
     {
+        /// <summary>
+        /// this methos is used for getting all the users from the database
+        /// </summary>
+        /// <returns>list of user</returns>
         List<User> getAllUser();
 
+        /// <summary>
+        /// This methos is used to add user in databse
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns>string wether user is added or not</returns>
         string addUser(User user);
 
+        /// <summary>
+        /// This methos is used to update the user in the databse
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns>string wether user is updated or not</returns>
         string updateUser(User user);
 
     }
