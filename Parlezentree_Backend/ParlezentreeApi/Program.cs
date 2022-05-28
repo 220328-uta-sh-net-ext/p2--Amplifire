342cf984ad13c326f0c013b09b9f30f295d5ec83
Harsh4598 <harsh.ht008@gmail.com>
2022-05-28 09:18:56 -0400

    change progeam.cs file

--- Parlezentree_Backend/ParlezentreeApi/Program.cs
+++ Parlezentree_Backend/ParlezentreeApi/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddScoped<IUserRepository, UserRepository>();
 var app = builder.Build();
 
 
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
